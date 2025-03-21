a69374d4f340428ed9934a0c7aa6bb1e4d695089
Derek Yuan <d5yuan@uwaterloo.ca>
2025-03-21 05:34:44 -0400

    Resolve "Article refactoring + db"

--- desktop/src/main/kotlin/article/entities/Article.kt
+++ desktop/src/main/kotlin/article/entities/Article.kt
@@ -1,7 +1,10 @@
 package article.entities
 
 import androidx.compose.ui.graphics.Path
-import kotlinx.serialization.*
+import kotlinx.serialization.Contextual
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import org.bson.codecs.kotlinx.ObjectIdSerializer
 import org.bson.types.ObjectId
 
@@ -85,6 +88,7 @@ data class CanvasBlock (
     @SerialName("_id")
     @Contextual override var id: ObjectId = ObjectId(),
     var paths: MutableList<Path> = mutableListOf<Path>(),
+    var height: Int = 200
 ) : ContentBlock() {
     override val blockType = BlockType.CANVAS
     override fun copyBlock(): ContentBlock {
