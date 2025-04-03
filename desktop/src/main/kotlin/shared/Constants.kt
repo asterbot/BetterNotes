6419a93f3353988b5af22d853440286474190681
Derek Yuan <d5yuan@uwaterloo.ca>
2025-04-03 03:42:04 -0400

    Resolve "add related article menu in article"

--- desktop/src/main/kotlin/shared/Constants.kt
+++ desktop/src/main/kotlin/shared/Constants.kt
@@ -1,19 +1,20 @@
 package shared
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.IconButtonColors
-import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material.TextFieldColors
+import androidx.compose.material.TextFieldDefaults.outlinedTextFieldColors
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 object Colors {
-    val lightTeal: Color = Color(0xffB1CCD3)
-    val medTeal: Color = Color(0xff2C666E)
-    val darkTeal: Color = Color(0xff07393C)
-    val black: Color = Color(0xff0a090C)
-    val white: Color = Color(0xffFFFCFD)
-    val lightGrey: Color = Color(0xffF0EDEE)
-    val darkGrey: Color = Color(0xff333333)
+    val veryLightTeal: Color = Color(0xFFF6FBFC)
+    val lightTeal: Color = Color(0xFFB1CCD3)
+    val medTeal: Color = Color(0xFF2C666E)
+    val darkTeal: Color = Color(0xFF07393C)
+    val black: Color = Color(0xFF0A090C)
+    val white: Color = Color(0xFFFFFCFD)
+    val lightGrey: Color = Color(0xFFE0E0E0)
+    val darkGrey: Color = Color(0xFF333333)
+    val errorColor: Color = Color(0xFFCF0000)
 }
 
 @Composable
@@ -25,6 +26,14 @@ fun textButtonColours(): ButtonColors {
 }
 
 @Composable
+fun transparentTextButtonColours(): ButtonColors {
+    return ButtonDefaults.buttonColors(
+        containerColor = Color.Transparent,
+        contentColor = Colors.medTeal
+    )
+}
+
+@Composable
 fun iconButtonColours(): IconButtonColors {
     return IconButtonDefaults.iconButtonColors(
         containerColor = Colors.medTeal,
@@ -32,3 +41,57 @@ fun iconButtonColours(): IconButtonColors {
         disabledContainerColor = Colors.medTeal.copy(alpha = .4f)
     )
 }
+
+@Composable
+fun outlinedTextFieldColours(): TextFieldColors {
+    return outlinedTextFieldColors(
+        focusedBorderColor = Colors.medTeal,
+        unfocusedBorderColor = Colors.darkGrey,
+        focusedLabelColor = Colors.medTeal,
+        unfocusedLabelColor = Colors.darkGrey,
+        cursorColor = Colors.black
+    )
+}
+
+@Composable
+fun textFieldColours() : androidx.compose.material3.TextFieldColors {
+    return TextFieldDefaults.colors(
+        cursorColor = Color.Black,
+        focusedLabelColor = Colors.darkGrey,
+        focusedIndicatorColor = Colors.medTeal,
+        unfocusedLabelColor = Colors.darkGrey,
+        errorIndicatorColor = Colors.errorColor,
+        errorLabelColor = Colors.errorColor,
+        errorCursorColor = Colors.errorColor,
+        unfocusedContainerColor = Colors.veryLightTeal,
+        focusedContainerColor = Colors.veryLightTeal.times(0.9f)
+    )
+}
+
+@Composable
+fun checkboxColours(): CheckboxColors {
+    return CheckboxDefaults.colors(
+        checkmarkColor = Color.White,
+        checkedColor = Colors.medTeal,
+        uncheckedColor = Colors.darkTeal
+    )
+}
+
+@Composable
+fun switchColours(): SwitchColors {
+    return SwitchDefaults.colors(
+        checkedThumbColor = Colors.medTeal,
+        checkedTrackColor = Colors.lightTeal,
+        checkedBorderColor = Colors.medTeal,
+        uncheckedThumbColor = Colors.lightGrey.times(0.7f),
+        uncheckedTrackColor = Color.Transparent,
+        uncheckedBorderColor = Colors.medTeal
+    )
+}
+
+
+fun Color.times(factor: Float) = copy(
+    red = (red * factor).coerceIn(0f, 1f),
+    green = (green * factor).coerceIn(0f, 1f),
+    blue = (blue * factor).coerceIn(0f, 1f),
+)
